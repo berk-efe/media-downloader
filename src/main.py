7f10502130a140f3179ee5fd5eb8f694988ebf7e
AkioKiyota <berkefekeskin@gmail.com>
2024-07-24 19:57:42 +0300

    start

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("CustomTkinter complex_example.py")
+        self.geometry(f"{1100}x{580}")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
